0bfa7347693d4ef073698dd882c9990deec44858
aglamkhujaev <aglamkhujaev@gmail.com>
2025-10-03 10:48:35 +0500

    feat: add desktop slider component

---
+++ src/components/desktop-slider/index.js
@@ -0,0 +1,75 @@
+class DesktopSlider extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+    connectedCallback() {
+    this.shadowRoot.innerHTML = `
+        <style>
+            .desktop-slider {
+                width: 480px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                gap: 20px;
+
+            }
+            .slider-image {
+                width: 100%;
+                height: 480px;
+            }
+            .slider-image ::slotted(img) {
+                width: 100%;
+                height: auto;
+                object-fit: cover;
+            }
+            .slider-content {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                gap: 16px;
+                text-align: center;
+            }
+            .slider-content ::slotted(h2) {
+                font-size: 24px;
+                font-weight: 600;
+                line-height: 125%;
+                color: var(--dark);
+                margin: 0;
+            }
+            .slider-content ::slotted(p) {
+                font-family: Inter, sans-serif;
+                font-size: 16px;
+                font-weight: 400;
+                line-height: 150%;
+                color: var(--dark);
+            }
+            .slider-content ::slotted(.price) {
+                font-size: 24px;
+                font-weight: 600;
+                color: var(--dark);
+            }
+
+            @media (max-width: 768px) {
+                .desktop-slider {
+                    width: 100%;
+                    padding: 0 20px;
+                }
+            }
+        </style>
+        <div class="desktop-slider">
+            <div class="slider-image">
+                <slot name="image"></slot>
+            </div>
+            <div class="slider-content">
+                <slot name="content-title"></slot>
+                <slot name="content-text"></slot>
+                <slot name="content-price"></slot>
+            </div>
+        </div>
+    `;
+  }
+}
+
+customElements.define('desktop-slider', DesktopSlider);
