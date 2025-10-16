2ddb16456b6de932ae51279f393eedc63b9543d1
baxromxoja16 <aglamof@mail.ru>
2025-10-16 11:51:41 +0500

    fix: carousel button migrate to ts

--- src/components/button-carousel/index.ts
+++ src/components/button-carousel/index.ts
@@ -1,12 +1,12 @@
 class CarouselButton extends HTMLElement {
+    shadow: ShadowRoot;
     constructor() {
         super();
-        this.attachShadow({ mode: 'open' });
+        this.shadow = this.attachShadow({ mode: 'open' });
     }
 
     connectedCallback() {
-        const type = this.getAttribute('type');
-        this.shadowRoot.innerHTML = `<style>
+        this.shadow.innerHTML = `<style>
             .carousel-button {
                 display: flex;
                 align-items: center;
@@ -39,22 +39,24 @@ class CarouselButton extends HTMLElement {
         </button>
         `;
 
-        const svgElement = this.shadowRoot.querySelector('slot[name="icon"]');
-        const buttonElement = this.shadowRoot.querySelector('.carousel-button');
+        const svgElement = this.shadow.querySelector('slot[name="icon"]') as HTMLSlotElement;
+        const buttonElement = this.shadow.querySelector('.carousel-button') as HTMLElement;
         if (svgElement) {
             const assignedNodes = svgElement.assignedNodes();
-            assignedNodes.forEach(node => {
-                const paths = node.querySelectorAll('path');
-                paths.forEach(p => {
-                    buttonElement.addEventListener('mouseover', () => {
-                        p.style.transition = '0.3s';
-                        p.style.stroke = 'var(--light)';
+            assignedNodes.forEach((node) => {
+                if (node instanceof Element) {
+                    const paths = node.querySelectorAll('path');
+                    paths.forEach((p) => {
+                        buttonElement.addEventListener('mouseover', () => {
+                            (p as SVGPathElement).style.transition = '0.3s';
+                            (p as SVGPathElement).style.stroke = 'var(--light)';
+                        });
+                        buttonElement.addEventListener('mouseout', () => {
+                            (p as SVGPathElement).style.transition = '0.3s';
+                            (p as SVGPathElement).style.stroke = 'var(--dark)';
+                        });
                     });
-                    buttonElement.addEventListener('mouseout', () => {
-                        p.style.transition = '0.3s';
-                        p.style.stroke = 'var(--dark)';
-                    });
-                });
+                }
             });
         }
     }
