10c9c3eb6bcfb9c08fa3c044532afff6b18672ac
baxromxoja16 <aglamof@mail.ru>
2025-10-28 12:18:51 +0500

    feat: add tab button and menu layout

--- src/app/pages/menu/menu.ts
+++ src/app/pages/menu/menu.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TabButton } from "../../components/tab-button/tab-button";
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [TabButton],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
 export class Menu {
-
+  loading: boolean = false;
 }
