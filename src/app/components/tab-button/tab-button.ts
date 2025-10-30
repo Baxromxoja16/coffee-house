ed6a0bcd75f10db46806c85e921ac3de5a0896a2
baxromxoja16 <aglamof@mail.ru>
2025-10-30 14:44:02 +0500

    feat: tab button active

--- src/app/components/tab-button/tab-button.ts
+++ src/app/components/tab-button/tab-button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tab-button',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './tab-button.css',
 })
 export class TabButton {
-
+  @Input() active = false;
 }
